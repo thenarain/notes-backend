add5b2fc378757fe87eb1622061bdc47e2a47bdb
Kartikay <kartiksrivastava19@gmail.com>
2023-04-27 10:19:21 +0530

    Add frontend Build

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors")
 const app = express();
 app.use(cors())
-
+app.use(express.static('build'))
 app.use(express.json());
 
 let notes = [
